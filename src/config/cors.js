d6f61f69d7a908cd57c85af8382cae96cfa23faf
Patrick Siqueira <patrickgsiqueira@hotmail.com>
2026-02-05 11:43:31 -0300

    fix: allow x-unit-id header in CORS for unit data isolation

--- src/config/cors.js
+++ src/config/cors.js
@@ -5,7 +5,7 @@ const corsOptions = {
     },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'x-unit-id'],
 };
 
 module.exports = corsOptions;
